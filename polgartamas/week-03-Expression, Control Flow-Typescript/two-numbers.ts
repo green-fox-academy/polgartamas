d298067e2067a140c90cdaa1e753204e375910bc
polgartamas <hello@polgartamas.hu>
2021-10-26 17:01:45 +0200

    making operations with two numbers

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/two-numbers.ts
@@ -0,0 +1,13 @@
+'use strict';
+let myNumber1 = 22;
+let myNumber2 = 13;
+let myAdded = (myNumber1+myNumber2);
+let mySubstracted = (myNumber1-myNumber2);
+let myMultiplied = (myNumber2*myNumber1);
+let myDivided = ((myNumber2/myNumber1).toFixed(2));
+
+console.log(myAdded);
+console.log(mySubstracted);
+console.log(myMultiplied);
+console.log(myDivided);
+
