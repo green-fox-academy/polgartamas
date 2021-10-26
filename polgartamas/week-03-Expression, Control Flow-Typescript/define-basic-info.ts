54010bbd4a9447ee8c9b82d976af4547c4e16d35
polgartamas <hello@polgartamas.hu>
2021-10-26 16:43:19 +0200

    printing some info about Tamás

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/define-basic-info.ts
@@ -0,0 +1,10 @@
+'use strict';
+let Name: string = "Tamás Polgár";
+let Age: number = 46;
+let Height: number = 1.83;
+let Married: boolean = false;
+
+console.log(Name);
+console.log(Age);
+console.log(Height);
+console.log(Married);
