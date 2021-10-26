1f788153bffb5eb18fc9177b7f0766a0cf83fd50
polgartamas <hello@polgartamas.hu>
2021-10-26 16:51:46 +0200

    print a test 100 times. I won't.

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+
+for (let t: number=0; t<100; t++){
+    console.log("I won\'t cheat on the exam!");
+}
