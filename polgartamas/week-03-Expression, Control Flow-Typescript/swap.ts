2652d76f3f549d373a6d302ef96a0eef20c6055f
polgartamas <hello@polgartamas.hu>
2021-10-26 17:00:41 +0200

    swap two numbers and print

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/swap.ts
@@ -0,0 +1,7 @@
+let numberToSwap1: number = 123;
+let numberToSwap2: number = 526;
+
+[numberToSwap1, numberToSwap2] = [numberToSwap2, numberToSwap1]
+
+console.log(numberToSwap1);
+console.log(numberToSwap2);
