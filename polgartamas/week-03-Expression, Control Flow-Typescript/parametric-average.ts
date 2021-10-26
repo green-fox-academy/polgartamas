343d5b8d20a6ff077f0a7e1bdcaaea05b4adfa3d
polgartamas <hello@polgartamas.hu>
2021-10-26 16:57:21 +0200

    adding numbers, counting average

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/parametric-average.ts
@@ -0,0 +1,14 @@
+'use strict';
+let e:number = 1;
+let f:string = "1";
+let g:number = 10;
+let mySum = f+g;
+let myAverage = ((e+g)/2);
+console.log("Sum: ", mySum);
+console.log("Averege: ", myAverage);
+
+
+
+// Write a program that calculates the sum and the average from 1 to a given number
+// Example input: 5
+// Example output: Sum: 15, Average: 3
