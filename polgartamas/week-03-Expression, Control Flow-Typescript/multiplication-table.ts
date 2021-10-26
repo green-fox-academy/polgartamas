b54683eabdb35e06073aaf5bd9507f1d193234b8
polgartamas <hello@polgartamas.hu>
2021-10-26 16:54:46 +0200

    make a multiplication table based on some options

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/multiplication-table.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+let number: number = 15;
+for (let h: number = 1; h<=10; h++){
+    const result = h*number;
+console.log (`${h} * ${number} = ${result}`);
+}
+
+
+
+// Create a program that
+// prints the multiplication table with number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
+
+export function multiplicationTable(number: number) {
+  // write your code here for Gradescope testing
+}
