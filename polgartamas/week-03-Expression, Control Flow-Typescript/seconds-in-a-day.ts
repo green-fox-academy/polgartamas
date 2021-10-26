864c53b8ce50313c021df1cb0c811dd29ca770b4
polgartamas <hello@polgartamas.hu>
2021-10-26 16:59:57 +0200

    counting remaining seconds from a specified one, in a day

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/seconds-in-a-day.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+
+let allSeconds = (24*60*60);
+console.log(allSeconds);
+let actualSeconds = ((14*60*60)+(34*60)+(42));
+console.log(allSeconds-actualSeconds);
