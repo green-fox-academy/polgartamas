337a9bb9e9d2f2d02ddb1c0bea1372907e2d4dea
polgartamas <hello@polgartamas.hu>
2021-10-26 16:42:32 +0200

    counting a cuboid

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/cuboid.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let x = 10;
+let y = 10;
+let z = 10;
+let surface: string = "Surfece Area: ";
+let volume: string = "Volume: ";
+console.log(surface+(6*x*x));
+console.log (volume+(x*x*x));
