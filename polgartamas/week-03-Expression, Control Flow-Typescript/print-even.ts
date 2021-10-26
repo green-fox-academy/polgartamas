b905777e367976181357aecf2f9c268563c27b06
polgartamas <hello@polgartamas.hu>
2021-10-26 16:58:10 +0200

    printing evens between 0 and 500

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/print-even.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+for (let p: number = 0; p < 500; p++){
+    if (p%2===0) {
+        console.log(p);
+    }
+}
