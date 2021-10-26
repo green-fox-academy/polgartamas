03984f7df5b26ddd4d17d20e393fbd9c6425ea70
polgartamas <hello@polgartamas.hu>
2021-10-26 16:52:49 +0200

    some practice

---
+++ polgartamas/week-03-Expression, Control Flow-Typescript/index.ts
@@ -0,0 +1,70 @@
+console.log("Hello world!");
+
+console.log("Hello, Tamás!");
+
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall.');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
+
+console.log("Hello, Jones!");
+console.log("Hello, Gergő!");
+console.log("Hello, Dávid!");
+
+// Prints a string to the terminal window
+let myString = "Hello world!";
+console.log (myString.length);
+
+// Prints an integer to the terminal window
+let myInteger = 42;
+console.log(myInteger);
+
+// Prints a floating point number to the terminal window
+let myFloatingnumber = Math.PI;
+console.log(myFloatingnumber);
+
+// Prints a boolean to the terminal window
+console.log(true);
+
+console.log(42);
+console.log(-1);
+console.log(0);
+console.log(12341234123412341234);
+
+console.log(3.1415);
+console.log(-1.5);
+console.log(0.0);
+
+console.log(.5)
+console.log(5.)
+
+console.log(1 + 1);
+console.log(5 - 2);
+console.log(3 * 4);
+console.log(6 / 2);
+console.log(5 / 2);
+console.log(5 % 2);
+
+console.log(true);
+console.log(false);
+
+console.log(!true);
+console.log(!false);
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log('apple');
+console.log('don\'t');
+console.log('"Everything you can imagine is real." - Picasso');
+
+console.log('tooth' + 'brush');
+console.log('My favorite number is: ' + 8);
+
