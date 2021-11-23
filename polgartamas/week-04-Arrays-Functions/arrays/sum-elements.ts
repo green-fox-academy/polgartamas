90b97115c6b313720c0be2c9ef1f3714e94e5048
polgartamas <hello@polgartamas.hu>
2021-11-23 14:45:44 +0100

    sum the elements of an array

---
+++ polgartamas/week-04-Arrays-Functions/arrays/sum-elements.ts
@@ -0,0 +1,6 @@
+// - Create a variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+let numbers1: number[] = [54, 23, 66, 12];
+console.log(numbers1[1] + numbers1[2]);
