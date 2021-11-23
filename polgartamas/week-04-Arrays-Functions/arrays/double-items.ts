883fa0fdd1ae08b56f79476ff3229c8a7c82accd
polgartamas <hello@polgartamas.hu>
2021-11-23 14:55:14 +0100

    double items of an array

---
+++ polgartamas/week-04-Arrays-Functions/arrays/double-items.ts
@@ -0,0 +1,11 @@
+// - Create an array variable named `drinks` with the following content:
+//   `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// - Double all the strings in the array, use a built in array method instead of
+//   a loop
+// - It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+drinks = drinks.map((element: string, i: number, array) => {
+  return element + element;
+});
+console.log(drinks);
