f7ded541c7b914845f27c4de1c3852e923516f6f
polgartamas <hello@polgartamas.hu>
2021-11-25 20:55:20 +0100

    print parameters with a function

---
+++ polgartamas/week-04-Arrays-Functions/functions/print-params.ts
@@ -0,0 +1,9 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+let myParams: number[] = [];
+function printParams(myParams: any): void {
+  console.log(myParams);
+}
+printParams([1, 2, 3, 4, 5, 6]);
