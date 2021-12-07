3d6a2c027eeb122c5127b1f49029b2a5aa7e0a60
polgartamas <hello@polgartamas.hu>
2021-12-07 22:09:50 +0100

    put a text between two words in a list

---
+++ polgartamas/week-05-Data-structures/takes-longer.ts
@@ -0,0 +1,11 @@
+'use strict';
+// While saving this quote, a disk error has occured. Please fix it!
+// Insert the words "always takes longer than" between the words "It" and "you"!
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+quote = [
+  quote.slice(0, quote.indexOf('you')),
+  'always takes longer than ',
+  quote.slice(quote.indexOf('you')),
+].join('');
+console.log(quote);
