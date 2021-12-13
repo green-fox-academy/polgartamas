c7b85994db4eb88fec41106a632ce3d5df3f7c9c
polgartamas <hello@polgartamas.hu>
2021-12-13 23:38:46 +0100

    create animals and they can eat and drink and play

---
+++ polgartamas/week-08-classes-objects/animal/animal.ts
@@ -0,0 +1,41 @@
+// Create an Animal class
+
+//     Every animal has a hunger value, which is a whole number
+//     Every animal has a thirst value, which is a whole number
+//     When creating a new animal instance these values must be set to the default 50 value
+//     Every animal can eat() which decreases its hunger by one
+//     Every animal can drink() which decreases its thirst by one
+//     Every animal can play() which increases both its hunger and thirst by one
+
+'use strict';
+
+class Animal {
+  name: string;
+  hunger: number;
+  thirst: number;
+
+  constructor(name: string) {
+    this.name = name;
+    this.hunger = 50;
+    this.thirst = 50;
+  }
+  eat(): void {
+    this.hunger--;
+  }
+  drink(): void {
+    this.thirst--;
+  }
+  play(): void {
+    this.hunger++ && this.thirst++;
+  }
+
+  printHungerAndThirst(): void {
+    console.log(this.hunger, this.thirst);
+  }
+}
+
+let dog = new Animal('dog');
+dog.eat();
+dog.printHungerAndThirst();
+dog.play();
+dog.printHungerAndThirst();
