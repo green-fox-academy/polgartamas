1302c946bccaa1d92168a711c7979d065e268eb2
polgartamas <hello@polgartamas.hu>
2021-12-13 23:55:21 +0100

    choose the effective pokemon from a list

---
+++ polgartamas/week-08-classes-objects/Pokemon/main.ts
@@ -0,0 +1,37 @@
+import { Pokemon } from './Pokemon';
+
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'grass', 'water');
+
+// Which pokemon should Ash use?
+
+// Implement the following `chooseEffective` function which will choose the most effective pokemon for Ash
+function chooseEffective(
+  pokemonOfAsh: Pokemon[],
+  wildPokemon: Pokemon
+): string {
+  for (let i = 0; i < pokemonOfAsh.length; i++) {
+    if (pokemonOfAsh[i].isEffectiveAgainst(wildPokemon)) {
+      return pokemonOfAsh[i].name;
+    }
+  }
+}
+
+console.log(`I choose you!, ${chooseEffective(pokemonOfAsh, wildPokemon)}`);
+
+function initializePokemon(): Pokemon[] {
+  return [
+    new Pokemon('Bulbasaur', 'grass', 'water'),
+    new Pokemon('Pikachu', 'electric', 'water'),
+    new Pokemon('Charizard', 'fire', 'grass'),
+    new Pokemon('Pidgeot', 'flying', 'fighting'),
+    new Pokemon('Kingler', 'water', 'fire'),
+  ];
+}
