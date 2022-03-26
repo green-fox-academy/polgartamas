8198d76de38d3750d097a3bab68011a19e6905e4
polgartamas <hello@polgartamas.hu>
2022-03-26 15:13:49 +0100

    made changes based on Tomis suggestions

--- polgartamas/week-17-reddit/reddit/server.js
+++ polgartamas/week-17-reddit/reddit/server.js
@@ -5,11 +5,14 @@ import mysql from 'mysql';
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
   console.log(`Connected to localhost:${PORT}`);
 });
 
-let conn = mysql.createConnection({
+const conn = mysql.createConnection({
   password: 'password',
   host: 'localhost',
   user: 'root',
@@ -23,8 +26,6 @@ conn.connect((err) => {
   }
   console.log('Connection established');
 });
-
-app.use(express.json());
 
 app.get('/hello', (_request, response) => {
   response.send('Hello world!');
@@ -41,17 +42,30 @@ app.get('/posts', (_request, response) => {
 });
 
 app.post('/posts', (request, response) => {
-  const title = request.body.title;
-  const url = request.body.url;
-  const score = request.body.score;
+  const { title, url } = request.body;
+  if (!title || !url) {
+    return response.status(400).send('Please provide title and url!');
+  }
   conn.query(
-    `INSERT INTO posts (title, url, timestamp, score) VALUES ("${title}", "${url}", CURRENT_TIMESTAMP,"${score}")`,
+    `INSERT INTO posts (title, url) VALUES (?,?)`,
+    [title, url],
     (err, posts) => {
       if (err) {
         console.log(err);
-        return response.status(500).send();
+        return response.status(500).send(Error);
       }
-      return response.json(posts);
+      conn.query(
+        `SELECT * FROM posts WHERE id=(?)`,
+        [posts.insertId],
+        (err, posts) => {
+          if (err) {
+            console.log(err);
+            response.status(500).send(mysqlError);
+            return;
+          }
+          return response.status(200).json(posts);
+        }
+      );
     }
   );
 });
@@ -59,27 +73,48 @@ app.post('/posts', (request, response) => {
 app.put('/posts/:id/upvote', (request, response) => {
   const { id } = request.params;
   conn.query(
-    `UPDATE posts SET score = score + 1 WHERE id = ${id}`,
-    (err, posts) => {
+    `UPDATE posts SET score = score + 1 WHERE id = (?)`,
+    [id],
+    (err, result) => {
       if (err) {
         console.log(err);
         return response.status(500).send();
       }
-      return response.json(posts);
+      if (result.affectedRows === 0) {
+        return response.status(404).send('Post not found');
+      }
+      conn.query(`SELECT * FROM posts WHERE id=(?)`, [id], (err, result) => {
+        if (err) {
+          console.log(err);
+          return response.status(500).json(mysqlError);
+        }
+        return response.status(200).json(result);
+      });
     }
   );
 });
 
 app.put('/posts/:id/downvote', (request, response) => {
   const { id } = request.params;
+
   conn.query(
-    `UPDATE posts SET score = score - 1 WHERE id = ${id}`,
+    `UPDATE posts SET score = score - 1 WHERE id = (?)`,
+    [id],
     (err, posts) => {
       if (err) {
         console.log(err);
         return response.status(500).send();
       }
-      return response.json(posts);
+      if (posts.affectedRows === 0) {
+        return response.status(404).send('Post not found');
+      }
+      conn.query(`SELECT * FROM posts WHERE id=(?)`, [id], (err, posts) => {
+        if (err) {
+          console.log(err);
+          return response.status(500).json(mysqlError);
+        }
+        return response.status(200).json(posts);
+      });
     }
   );
 });
